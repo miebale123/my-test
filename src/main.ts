346f2807bf793d9a4c49ca5df130a7a00d888e55
miebale123 <nigusmiebale@gmail.com>
2025-10-28 11:50:02 +0300

    port change

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4444);
+  await app.listen(process.env.PORT ?? 4444, '0.0.0.0');
 
   console.log('hello world');
 }
